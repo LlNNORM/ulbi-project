c405e6f6edfa3225c881a26f7fc685e89d815696
Ivan <linnorm.ya.ru>
2024-04-11 20:03:50 +0300

    Loader is made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import './styles/App.css';
-import { useState } from "react";
-import { useMemo } from "react";
-import PostList from "./components/PostList";
 
+import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./UI/MyModal/MyModal";
 import MyButton from "./UI/button/MyButton";
+import PostService from "./API/PostService";
 import { usePosts } from "./hooks/usePosts";
+import Loader from "./UI/Loader/Loader";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
 
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id:1, title:'Javascript 1',body:'Description'},
-    {id:2, title:'Javascript 2',body:'Description'},
-    {id:3, title:'Javascript 3',body:'Description'},
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({sort:'', query:''});
   // чтобы сортировка не происходила каждый раз по новой воспользуемся мемоизацией 
@@ -24,11 +24,23 @@ function App() {
   // если мы удаляем или добавляем пост,а также меняем алгоритм сортировки, 
   // но не при вводе в строку поиска,как это было прежде. 
   const [modal, setModal] = useState(false)
+  const [isPostsLoading, setIsPostsLoading]=useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  }
+
+  async function fetchPosts() {
+    setIsPostsLoading(true)
+    const posts = await PostService.getAll()
+    setPosts(posts)
+    setIsPostsLoading(false)
   }
 
   const removePost = (post) => {
@@ -47,7 +59,11 @@ function App() {
         <hr style={{margin: '15px 0'}}/>
         <PostFilter filter={filter} setFilter={setFilter}/>
         {/* Условная отрисовка: */}
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов 1:'/>
+        {isPostsLoading 
+              ?  <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div> 
+              :  <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов 1:'/>
+        }      
+       
     </div>
   );
 }
